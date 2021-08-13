a817bf635357fd6b5a7da0f90f2f9d7bea842c04
sumit45doc <ss8559283@gmail.com>
2021-08-13 13:01:52 +0530

    flutter-dice

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.teal,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceButton = 1;
+  int rightDiceButton = 1;
+  void diceButton() {
+    setState(() {
+      leftDiceButton = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                diceButton();
+              },
+              child: Image.asset('images/dice$leftDiceButton.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
